424e008c88a07ab9519f43ae7f6a0e7b39290cd4
Heather Anderson & Sarah Joy Calpo <student@epicodus.com>
2015-01-08 12:12:04 -0800

    Created layout, set-up app.rb, added more view files, renamed form.erb

---
+++ app.rb
@@ -0,0 +1,47 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/scrabble')
+
+
+get('/') do
+  erb(:home)
+end
+
+#Practice
+
+get('/practice') do #This is the form with 2 inputs
+  erb(:practice)
+end
+
+get('score') do # Results page for practice
+    #"Hey (name) why don't you have friends?"
+    # if have time, add Time comments (ie "why are you awake?")
+    @score = params.fetch('word').scrabble()
+    @name = params.fetch('name')
+  erb(:score)
+end
+
+#Two player
+
+get('/two_player') do   #This is the form with 4 things of input
+  erb(:two_player)
+end
+
+get('/two_player_winner') do # Results page for 4 input form
+  @word1 = params.fetch('word')
+  @score1 = params.fetch('word').scrabble()
+
+  @word2 = params.fetch('word')
+  @score2 = params.fetch('word').scrabble()
+
+  if @score1.>(@score2)
+    @winner = params.fetch('player1')
+  elsif @score1.==(@score2)
+    @winner = "No one. It was a tie!"
+  else
+    @winner = params.fetch('player2')
+  end
+
+  erb(:two_player_winner)
+end
